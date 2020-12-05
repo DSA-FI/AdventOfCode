10a5cc6d6da2c892fe34bc984bd517f23bf8c848
DSA <75334986+DSA-FI@users.noreply.github.com>
2020-12-05 13:43:02 +0200

    Update Day3_part1.py

--- Day3/Day3_part1.py
+++ Day3/Day3_part1.py
@@ -1,30 +1,29 @@
 
-
 #Code for the day 3 part 1 of Advent For Code -challenge
 
 SlopeMap = open("Day3\\Day3_Data.txt", 'r')
 
 Tree_Counter = 0
 IgnoreFirstTree = True
-counter = 0
+PositionCounter = 0
 
 for line in SlopeMap:
 
     line = line.rstrip()
-    SquareArray = line
+    map_of_current_line = line
     
 
-    while len(SquareArray) < counter:
-        
-        SquareArray = SquareArray + line
+    while len(map_of_current_line) <= PositionCounter:
+        map_of_current_line = map_of_current_line + line
 
 
 
-    if SquareArray[counter] == "#" and IgnoreFirstTree == False:
+    if map_of_current_line[PositionCounter] == "#" and IgnoreFirstTree == False:
         Tree_Counter = Tree_Counter + 1
 
+     
     IgnoreFirstTree = False
-    counter = counter + 3
+    PositionCounter = PositionCounter + 3
 
 
 print("\nOn current path you encounter", Tree_Counter, "trees.\n")
